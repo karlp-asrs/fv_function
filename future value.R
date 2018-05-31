39e81010034ec9324f3ed1d622fc8c333414fdb7
karlp-asrs <karlp@azasrs.gov>
2018-05-31 09:47:01 -0700

    another correct to comment format

--- future value.R
+++ future value.R
@@ -1,21 +1,21 @@
 fv.tr=function(CF,TR,ValDate=NULL) {  
-#`
-#` DESCRIPTION:
-#` a function that calculates future value with time varying interest rates
-#` 
-#` ARGUMENTS:
-#` CF -- a zoo object of cash flows
-#` TR -- a zoo object which is a total return index reflecting the variable interest rate, 
-#`       there must be an entry in TR for every date in CF and for the ValDate
-#` ValDate -- the valuation date or dates, defaults to last date in cash flow if none provided,
-#`             if earlier than last date in cash flows, then cash flows after this date are ignored,
-#`             if earlier than first date in cash flows, returns NA
-#`             defaults to the last date in the cash flow,
-#`             must be in the same date format as CF and TR
-#`
-#` RESULT:
-#` a zoo object of the same length as ValDate which is the value of the cash flows as if invested in the index
-#` as of each valuation date
+#
+# DESCRIPTION:
+# a function that calculates future value with time varying interest rates
+# 
+# ARGUMENTS:
+# CF -- a zoo object of cash flows
+# TR -- a zoo object which is a total return index reflecting the variable interest rate, 
+#       there must be an entry in TR for every date in CF and for the ValDate
+# ValDate -- the valuation date or dates, defaults to last date in cash flow if none provided,
+#             if earlier than last date in cash flows, then cash flows after this date are ignored,
+#             if earlier than first date in cash flows, returns NA
+#             defaults to the last date in the cash flow,
+#             must be in the same date format as CF and TR
+#
+# RESULT:
+# a zoo object of the same length as ValDate which is the value of the cash flows as if invested in the index
+# as of each valuation date
 #
   if(is.null(ValDate)) ValDate=tail(time(CF),1)
   if(!is.zoo(CF)) stop("CF not a zoo object")
